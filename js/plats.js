13c32e92cd316a10e0c90571cbacb31c47afa57f
Boulogne <theo.boulogne@isen.yncrea.fr>
2019-11-29 09:21:09 +0100

    Initialisation
    Upload de l'ancien tp

---
+++ js/plats.js
@@ -0,0 +1,10 @@
+let plats = [
+    { name: 'Quesadillas', preparation: '30', ingrédients: ['17', '13', '20', '18'], prixPreparation: '3' },
+    { name: 'Croque Monsieur Saumon Boursin', preparation: '15', ingrédients: ['19', '6', '16'], prixPreparation: '2' },
+    { name: 'Pates Carbonara', preparation: '15', ingrédients: ['0', '3', '4', '5', '22'], prixPreparation: '6' },
+    { name: 'Pates Bolognaises', preparation: '25', ingrédients: ['0', '1', '2', '22'], prixPreparation: '8' },
+    { name: 'Salade composée', preparation: '5', ingrédients: ['12', '13', '14', '15'], prixPreparation: '1' },
+    { name: 'Oeuf saumon en cocotte', preparation: '30', ingrédients: ['5', '6', '4', '7'], prixPreparation: '6' },
+    { name: 'Pancakes', preparation: '14', ingrédients: ['21', '8', '20', '5'], prixPreparation: '5' },
+    { name: 'Brownies', preparation: '45', ingrédients: ['5', '8', '9', '10', '11', '20'], prixPreparation: '8' },
+];
